1683cdabfae7b6c7fc169a67b1a8aaf75be27c61
Jenny Khranovska <jenny.khranovska.syne25lin@edu.tucsweden.se>
2025-09-26 18:03:21 +0200

    Add project files.

---
+++ Dagbok1/DiaryEntry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dagbok
+{
+    public class DiaryEntry
+    {
+        public DateTime Date { get; set; } //  kan hämtas och läsas av andra 
+        public string Text { get; set; }
+    }
+
+}
---
+++ Dagbok1/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Dagbok1
+{
+        internal class Program
+        {
+
+
+
+            static void Main(string[] args)
+
+
+
+            {
+                Console.WriteLine("------------");
+                Console.WriteLine("Min dagbok");
+                Console.WriteLine("-------------");
+
+                while (true)
+                {
+
+                    Console.WriteLine("\nVad vill du skriva om idag?");
+                    Console.WriteLine();
+                    Console.WriteLine("1. Dagens kännsla?");
+                    Console.WriteLine("2.Skoldagen – vad gjorde du?");
+                    Console.WriteLine("3. Något roligt eller oväntat som hände?");
+                    Console.WriteLine("4. Visa tidigare inlägg");
+                    Console.WriteLine("5. Avsluta dagboken");
+
+                    Console.Write("\nDitt val: ");
+                    string input = Console.ReadLine();
+
+                    switch (input)
+                    {
+
+
+
+
+
+
+                    }
+                    List<DiaryEntry> diaryEntries = new List<DiaryEntry>();
+
+
+
+
+
+
+
+
+
+
+                }
+            }
+        }
+    }
+
+
